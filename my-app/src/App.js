8e8fc9f4bdb2320ddf1181d2b42c843007ad8110
Julian Graves <Juliangraves96@gmail.com>
2021-09-22 16:17:23 -0700

    added navabar as well as routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,21 +8,17 @@ function App() {
   const [navBarLinks] = useState(["About Me", "Porfollio", "Contacts", "Resume"]);
   const [activeNav, setActiveNav] = useState("About Me");
   return (
-    <div className="Portfollio">
-      <header className="Julian Graves">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          weeedemmboissss.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Nav />
+        <main>
+            <Switch>
+              <Route exact path= '/about' component={ About }/>
+              <Route exact path= '/test' component={ Test }/>
+              <Route component={ Homepage }/>
+            </Switch>
+        </main>
+      </Router>
     </div>
   );
 }
